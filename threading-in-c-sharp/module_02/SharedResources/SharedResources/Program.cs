c28b6de78f39f1e1d05449ba0235b7908f1cba2a
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-05 15:38:43 +0200

    Threading in C#. 2 - Introduction to Threading: Shared Resources

---
+++ threading-in-c-sharp/module_02/SharedResources/SharedResources/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace SharedResources
+{
+    public class Program
+    {
+        private static bool isCompleted;
+        static readonly object lockCompleted = new object();
+
+        public static void Main(string[] args)
+        {
+            Thread thread = new Thread(HelloWorld);
+            thread.Start();
+
+            HelloWorld();
+        }
+
+        private static void HelloWorld()
+        {
+            lock (lockCompleted)
+            {
+                if (!isCompleted)
+                {
+                    isCompleted = true;
+                    Console.WriteLine("Hello world should be printed only once");
+                }
+            }
+        }
+    }
+}
