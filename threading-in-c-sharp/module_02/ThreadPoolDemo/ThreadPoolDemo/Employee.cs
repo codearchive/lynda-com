1a973a0b15cb1136619a88043ff8a49c260cedb9
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-05 17:05:24 +0200

    Threading in C#. 2 - Introduction to Threading: Thread Pool Demo

---
+++ threading-in-c-sharp/module_02/ThreadPoolDemo/ThreadPoolDemo/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace ThreadPoolDemo
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+
+        public Employee(string name, string companyName)
+        {
+            Name = name;
+            CompanyName = companyName;
+        }
+    }
+}
---
+++ threading-in-c-sharp/module_02/ThreadPoolDemo/ThreadPoolDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("1: " + Thread.CurrentThread.IsThreadPoolThread);
+
+            Employee employee = new Employee("Pavel", "RideOn");
+            ThreadPool.QueueUserWorkItem(new WaitCallback(DisplayEmployeeInfo), employee);
+
+            //var processorCount = Environment.ProcessorCount;
+            //ThreadPool.SetMaxThreads(processorCount * 2, processorCount * 2);
+            int workerThreads = 0;
+            int completionPortThreads = 0;
+            ThreadPool.GetMinThreads(out workerThreads, out completionPortThreads);
+
+            ThreadPool.SetMaxThreads(workerThreads * 2, completionPortThreads * 2);
+            Console.WriteLine("2: " + Thread.CurrentThread.IsThreadPoolThread);
+            Console.ReadLine();
+        }
+
+        private static void DisplayEmployeeInfo(object employeeObject)
+        {
+            Console.WriteLine("3: " + Thread.CurrentThread.IsThreadPoolThread);
+            Employee employee = employeeObject as Employee;
+            Console.WriteLine($"Person name is {employee.Name}. Company name is {employee.CompanyName}");
+
+        }
+    }
+}
