c0ca7e5f5f225e9698dac771f204dd3e0ad928fc
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-05 15:04:54 +0200

    Threading in C#. 2 - Introduction to Threading: Context Switching

---
+++ threading-in-c-sharp/module_02/ContextSwitching/ContextSwitching/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace ContextSwitching
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread thread = new Thread(WriteUsingNewThread);
+            thread.Name = "Worker Thread";
+            Thread.CurrentThread.Name = "Main Thread";
+            thread.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(" A" + i + " ");
+                Thread.Sleep(100);
+            }
+        }
+
+        private static void WriteUsingNewThread()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(" Z" + i + " ");
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
