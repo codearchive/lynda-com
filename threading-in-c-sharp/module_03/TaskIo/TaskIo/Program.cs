d6c2982adf6ab19ccca9c9cae1e8ebeb5ffe388d
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-06 16:32:29 +0200

    Threading in C#. 3 - Tasks: Task with IO

---
+++ threading-in-c-sharp/module_03/TaskIo/TaskIo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskIo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Task<string> task = Task.Factory.StartNew<string>(() => GetPosts("https://jsonplaceholder.typicode.com/posts"));
+
+            SomethingElse();
+
+            //task.Wait();
+            try
+            {
+                Console.WriteLine(task.Result);
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void SomethingElse()
+        {
+            // Dummy implementation
+        }
+
+
+        private static string GetPosts(string url)
+        {
+            //throw null;
+            using (var client = new System.Net.WebClient())
+            {
+                return client.DownloadString(url);
+            }
+        }
+    }
+}
