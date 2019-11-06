a3769d7f590a60e8b2c7ac11e2ca7ce24fea901e
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-06 13:46:52 +0200

    Threading in C#. 3 - Tasks: Task Introduction

---
+++ threading-in-c-sharp/module_03/TaskIntro/TaskIntro/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskIntro
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Task task = new Task(SimpleMethod);
+            task.Start();
+
+            Task<string> taskThatReturns = new Task<string>(MethodThatReturns);
+            taskThatReturns.Start();
+            taskThatReturns.Wait();
+            Console.WriteLine(taskThatReturns.Result);
+
+            Console.ReadLine();
+        }
+
+        private static string MethodThatReturns()
+        {
+            Thread.Sleep(2000);
+            return "Hi again";
+        }
+
+        private static void SimpleMethod()
+        {
+            //Thread.Sleep(5000);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
