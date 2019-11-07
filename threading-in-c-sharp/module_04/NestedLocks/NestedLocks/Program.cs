b7b89d9ef3642a37b76950ddebeb9330bf7ddded
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-07 23:42:02 +0200

    Threading in C#. 4 - Synchronization: Nested locks

---
+++ threading-in-c-sharp/module_04/NestedLocks/NestedLocks/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NestedLocks
+{
+    public class Program
+    {
+        public static object newLock = new object();
+
+        public static void Main()
+        {
+            lock (newLock)
+            {
+                DoSomething();
+            }
+        }
+
+        private static void DoSomething()
+        {
+            lock (newLock)
+            {
+                Task.Delay(2000);
+                lock (newLock)
+                {
+                    Console.WriteLine("HEllo");
+                    AnotherMethod();
+                }
+            }
+        }
+
+        private static void AnotherMethod()
+        {
+            lock (newLock)
+            {
+                Console.WriteLine("World");
+            }
+        }
+    }
+}
