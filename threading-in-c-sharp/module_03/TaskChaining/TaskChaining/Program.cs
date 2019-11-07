2461137c40f0017e531828fbe9131b7818814916
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-07 23:18:45 +0200

    Threading in C#. 4 - Synchronization: Monitor and locks

---
+++ threading-in-c-sharp/module_03/TaskChaining/TaskChaining/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskChaining
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            Task<string> antecedent = Task.Run(() =>
+            {
+                Console.WriteLine("What day is today?");
+                Task.Delay(5000);
+                return DateTime.Today.ToShortDateString();
+            });
+
+            Task<string> continuation = antecedent.ContinueWith(x =>
+            {
+                return "Today is " + antecedent.Result;
+            });
+            Console.WriteLine("This will display before the result");
+            Console.WriteLine(continuation.Result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ threading-in-c-sharp/module_04/LocksAndMonitor/LocksAndMonitor/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LocksAndMonitor
+{
+    internal class Account
+    {
+        private Object newLock = new object();
+
+        private int _initialBalance;
+
+        Random random = new Random();
+
+        public Account(int initialBalance)
+        {
+            _initialBalance = initialBalance;
+        }
+
+        int Withdraw(int amount)
+        {
+            if (_initialBalance < 0)
+            {
+                throw new Exception("Not enough balance");
+            }
+
+            Monitor.Enter(newLock);
+            try
+            {
+                if (_initialBalance >= amount)
+                {
+                    Console.WriteLine($"Amount drawn: {amount}");
+                    _initialBalance -= amount;
+
+                    return _initialBalance;
+                }
+            }
+            finally
+            {
+                Monitor.Exit(newLock);
+            }
+
+            return 0;
+        }
+
+        public void WithdrawRandomly()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var balance = Withdraw(random.Next(2000, 5000));
+
+                if (balance > 0)
+                {
+                    Console.WriteLine($"Balance left: {balance}");
+                } 
+            }
+        }
+    }
+}
