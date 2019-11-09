88f2f312cce94961b433ac4ba49b871858ffca33
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-09 23:29:05 +0200

    Threading in C#. 4 - Synchronization: Dead lock

---
+++ threading-in-c-sharp/module_04/Deadlocks/Deadlocks/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Deadlocks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            object firstLock = new object();
+            object secondLock = new object();
+
+            new Thread(() =>
+            {
+                lock (firstLock)
+                {
+                    Console.WriteLine("First Lock obtained");
+                    Thread.Sleep(2000);
+                    lock (secondLock)
+                    {
+                        Console.WriteLine("Second Lock obtained");
+                    }
+                }
+            }).Start();
+            lock (secondLock)
+            {
+                Console.WriteLine("Main Thread obtained Second Lock - 1");
+                Thread.Sleep(2000);
+                lock (firstLock)
+                {
+                    Console.WriteLine("Main Thread obtained Second Lock - 2");
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
