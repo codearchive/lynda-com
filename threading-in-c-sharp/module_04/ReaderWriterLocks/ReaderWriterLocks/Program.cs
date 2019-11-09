93b07af70afc2352382ff6fe1d492bb3ab3e6888
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-09 23:50:28 +0200

    Threading in C#. 4 - Synchronization: Reader/Writer lock

---
+++ threading-in-c-sharp/module_04/ReaderWriterLocks/ReaderWriterLocks/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReaderWriterLocks
+{
+    public class Program
+    {
+        static ReaderWriterLockSlim readerWriterLockSlim = new ReaderWriterLockSlim();
+        static Dictionary<int, string> persons = new Dictionary<int, string>();
+        static Random random = new Random();
+
+
+        public static void Main()
+        {
+            var task1 = Task.Factory.StartNew(Read);
+            var task2 = Task.Factory.StartNew(Write, "First User");
+            var task3 = Task.Factory.StartNew(Write, "Second User");
+            var task4 = Task.Factory.StartNew(Read);
+            var task5 = Task.Factory.StartNew(Read);
+            Task.WaitAll(task1, task2, task3, task4, task5);
+        }
+
+        public static void Read()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                readerWriterLockSlim.EnterReadLock();
+                Thread.Sleep(50);
+                Console.WriteLine("reading" + i);
+                readerWriterLockSlim.ExitReadLock();
+            }
+        }
+
+        public static void Write(object user)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int id = GetRandom();
+                readerWriterLockSlim.EnterWriteLock();
+                var person = "Person " + i;
+                persons.Add(id, person);
+                readerWriterLockSlim.ExitWriteLock();
+                Console.WriteLine(user + " added " + person);
+                Thread.Sleep(250);
+            }
+        }
+
+        static int GetRandom()
+        {
+            lock (random)
+            {
+                return random.Next(2000, 5000);
+            }
+        }
+    }
+}
