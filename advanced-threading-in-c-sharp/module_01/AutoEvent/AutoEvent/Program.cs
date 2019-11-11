d888a5339909ba2d364b8699034894b29cdc1329
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-11 15:44:59 +0200

    Advanced threading in C#. 1 - Signaling and Thread Concepts: Signaling - AutoResetEvent

---
+++ advanced-threading-in-c-sharp/module_01/AutoEvent/AutoEvent/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoEvent
+{
+    public class Program
+    {
+        //static EventWaitHandle autoResetEvent = new EventWaitHandle(false, EventResetMode.AutoReset);
+        static EventWaitHandle eventWaitHandle = new AutoResetEvent(false);
+
+        public static void Main(string[] args)
+        {
+            Task.Factory.StartNew(WorkerThread);
+            Thread.Sleep(2500);
+            eventWaitHandle.Set();
+            Console.ReadLine();
+        }
+
+        private static void WorkerThread()
+        {
+            Console.WriteLine("Waiting to enter the gate");
+            eventWaitHandle.WaitOne();
+            Console.WriteLine("Gate was entered");
+        }
+    }
+}
